81fabf1a065ed16d624e7a0086f2ae57f195381e
sagadevanmi <sagadevanmi@gmail.com>
2020-12-06 19:59:53 +0530

    Initial 2 layouts done

---
+++ app/src/main/java/com/sdk/medicinereminder/AddReminderActivity.java
@@ -0,0 +1,14 @@
+package com.sdk.medicinereminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+    }
+}
